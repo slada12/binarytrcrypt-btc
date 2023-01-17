3da6f5fbb98774688124467c3e82da6f213bae71
Emmanuel Worgu <emmanuelworgu@gmail.com>
2023-01-17 13:47:50 -0800

    added chatbot

---
+++ chat.js
@@ -0,0 +1,39 @@
+const siteName = document.location.host;
+if (document.getElementById('site-name') === null) {
+  console.log('Nothing here');
+} else {
+  document.getElementById('site-name').textContent = siteName;
+}
+
+// (function blackList () {
+//   const blackListed = localStorage.getItem('blackList');
+
+//   // if (blackListed === null)
+
+//   if (blackListed === 'true' || blackListed !== null) {
+//     console.log(blackListed);
+//     document.location.href = '/user.html';
+//   }
+// } ());
+
+
+if (document.location.origin === 'https://octaprotraders.online/') {
+  console.log('LocalHost');
+
+  var script = document.createElement('script');
+
+  script.onload = function () {
+    console.log('Loading Script...');
+  }
+  script.src = "https://code.tidio.co/x7lqctxwoizauxp5d6dry4sq3ly4x928.js";
+
+  document.head.appendChild(script);
+
+      // <script src="//code.tidio.co/q80hpvc9ytpns2itn9kyxwwbv7hvcztn.js" async></script>
+} else if (document.location.origin === 'https://binarytrcrypt.com') {
+  var script = document.createElement('script');
+  script.src = "https://code.tidio.co/q80hpvc9ytpns2itn9kyxwwbv7hvcztn.js";
+  document.head.appendChild(script);
+} else {
+  return null;
+}
