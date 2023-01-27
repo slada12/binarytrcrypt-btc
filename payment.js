cc3e7b5a8fcb0f18b90a68b0d1e4ee8794dcc733
Emmanuel Worgu <emmanuelworgu@gmail.com>
2023-01-26 16:09:06 -0800

    fixed wallet address issue

--- payment.js
+++ payment.js
@@ -6,6 +6,42 @@
   document.getElementById('tow').textContent = `${wallet} Address:`;
   const copy = document.getElementById('copyWallet');
 
+
+  // Wallet address for octaprotraders.online
+  if (siteName === 'octaprotraders.online') {
+    console.log('Working for only octaprotraders...');
+    if ('USDT' === wallet.trim()) {
+      return copy.value = '0x6a5f385b7529c48298ed462927b7bcb742f84c5d'
+    }
+  
+    if ('Ethereum' === wallet.trim()) {
+      return copy.value = '0x6a5f385b7529c48298ed462927b7bcb742f84c5d'
+    }
+  
+    if ('Bitcoin' === wallet.trim()) {
+      return copy.value = '0x6a5f385b7529c48298ed462927b7bcb742f84c5d'
+    }
+  }
+
+
+  // Wallet address for elitecryptoinvestors.com
+  if (siteName === 'elitecryptoinvestors.com') {
+    console.log('Working for only octaprotraders...');
+    if ('USDT' === wallet.trim()) {
+      return copy.value = 'TYyRrU3hQp4wdbpyoLhJ9juUiHL2Jj4YHi'
+    }
+  
+    if ('Ethereum' === wallet.trim()) {
+      return copy.value = '0x54F33c33B39fdc09D34e665574F695FcBbe04D4F'
+    }
+  
+    if ('Bitcoin' === wallet.trim()) {
+      return copy.value = '1AfwJFfUH8gdyZBHYT8FQYXYRzxuEckCEm'
+    }
+  }
+
+
+  // Wallet address for other sites
   if ('USDT' === wallet.trim()) {
     return copy.value = '0x43e1f7772b826c6cc0f07476a5cc32e340da016e'
   }
--- siteName.js
+++ siteName.js
@@ -3,13 +3,26 @@
 // const siteName1 = document.location.hostname;
 
 const text = document.getElementsByClassName('sitename');
-console.log(text);
+const siteNumber = document.getElementById('sitenumber');
+// console.log(text);
 
 for (let i = 0; i < text.length; i++) {
-    console.log(text[i])
+    // console.log(text[i])
 
     text[i].textContent = siteName;
     
     // const element = array[index];
     
 }
+
+(function siteNumberFunc() {
+    if (siteNumber === null) {
+        return null;
+    }
+
+    if (siteName === 'elitecryptoinvestors.com') {
+        siteNumber.textContent = '+1(479)485-1098'
+    } else {
+        return;
+    }
+} ());
